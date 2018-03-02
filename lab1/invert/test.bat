430df182875f444d2fe0753822316098b2a8c09a
mpaymetov <muxa147@gmail.com>
2018-03-03 00:24:24 +0300

    исправления по замечания по программе Invert #2

--- lab1/invert/test.bat
+++ lab1/invert/test.bat
@@ -6,10 +6,16 @@ rem
 if NOT ERRORLEVEL 1 GOTO err
 
 rem 
-%PROGRAM% matrix.txt
+%PROGRAM% matrix_in1.txt > matrix_out_1.txt
 if ERRORLEVEL 1 GOTO err
-rem fc.exe "%TEMP%\fox.txt" test-data\fox.txt >nul
-rem if ERRORLEVEL 1 GOTO err
+fc.exe matrix_out_1.txt matrix_out_et1.txt > nul
+if ERRORLEVEL 1 GOTO err
+
+rem 
+%PROGRAM% matrix_in2.txt > matrix_out_2.txt"
+if ERRORLEVEL 1 GOTO err
+fc.exe matrix_out_2.txt matrix_out_et2.txt
+if ERRORLEVEL 1 GOTO err
 
 echo OK
 exit 0
