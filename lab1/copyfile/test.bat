bb839f845e2577ccf2561f2aba316bc51fc6f9ac
mpaymetov <muxa147@gmail.com>
2018-03-16 23:42:22 +0300

    Добавил тесты, добавил константность для ссылки в парамертах функции #4

--- lab1/copyfile/test.bat
+++ lab1/copyfile/test.bat
@@ -3,13 +3,16 @@ set PROGRAM="%~1"
 %PROGRAM% > nul
 if NOT ERRORLEVEL 1 GOTO err
 
-%PROGRAM% 256
+%PROGRAM% 256 > nul
 if NOT ERRORLEVEL 1 GOTO err
 
-%PROGRAM% Hello World
+%PROGRAM% Hello World > nul
 if NOT ERRORLEVEL 1 GOTO err
 
-%PROGRAM% test_in1.txt test_out1.txt
+%PROGRAM% test_in1.txt test_out1.txt > nul
+if ERRORLEVEL 1 GOTO err
+
+fc.exe test_in1.txt test_out1.txt > nul
 if ERRORLEVEL 1 GOTO err
 
 echo OK
