b834cf010095e1cdc9625c67776a71dc85b877c5
mpaymetov <muxa147@gmail.com>
2018-04-28 01:22:37 +0300

    исправил замечания #6

--- lab2/PrimeNumbers/PrimeNumbers.h
+++ lab2/PrimeNumbers/PrimeNumbers.h
@@ -2,6 +2,9 @@
 
 bool GetNumber(const std::string &str, int& number);
 
-std::vector<bool> GeneratePrimeNumbersVector(int upperBound);
+std::vector<bool> GenerateIsPrimeVector(int upperBound);
 
 std::set<int> GeneratePrimeNumbersSet(int upperBound);
+
+void PrintPrimeNumbers(const std::set<int>& primeNumbers);
+
--- lab2/PrimeNumbers/main.cpp
+++ lab2/PrimeNumbers/main.cpp
@@ -13,13 +13,24 @@ int main(int argc, char* argv[])
 	int upperBound = 0;
 	if (!GetNumber(argv[1], upperBound))
 	{
-		std::cout << "Invalid number of arguments.\n";
+		std::cout << "Invalid argument.\n";
 		std::cout << "Usage: PrimeNumbers.exe <upperBound 1..10^8>\n";
 		return 1;
 	}
 
-	std::set<int> primeNumbersSet = GeneratePrimeNumbersSet(upperBound);
-	std::cout << "to " << upperBound << " contains " << primeNumbersSet.size() << " prime numbers\n";
+	const int upperBoundMax = 100000000;
+	if (upperBound < 1 || upperBound > upperBoundMax)
+	{
+		std::cout << "Invalid argument.\n";
+		std::cout << "Usage: PrimeNumbers.exe <upperBound 1..10^8>\n";
+		return 1;
+	}
 
+	std::set<int> primeNumbersSet = {};
+	#ifndef _DEBUG
+		primeNumbersSet = GeneratePrimeNumbersSet(upperBound);
+	#endif
+	std::cout << "to " << upperBound << " contains " << primeNumbersSet.size() << " prime numbers:\n";
+	PrintPrimeNumbers(primeNumbersSet);
 	return 0;
 }
