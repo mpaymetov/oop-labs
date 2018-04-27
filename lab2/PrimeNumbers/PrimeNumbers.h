427477a72294f406098f9175448273e0aa7d41d4
mpaymetov <muxa147@gmail.com>
2018-04-27 23:24:01 +0300

    изменил программу и добавил тесты

--- lab2/PrimeNumbers/PrimeNumbers.h
+++ lab2/PrimeNumbers/PrimeNumbers.h
@@ -1,3 +1,7 @@
 #pragma once
 
+bool GetNumber(const std::string &str, int number);
+
+std::vector<bool> GeneratePrimeNumbersVector(int upperBound);
+
 std::set<int> GeneratePrimeNumbersSet(int upperBound);
--- lab2/PrimeNumbers/main.cpp
+++ lab2/PrimeNumbers/main.cpp
@@ -1,8 +1,6 @@
 #include "stdafx.h"
 #include "PrimeNumbers.h"
 
-constexpr int upperBoundMax = 100000000;
-
 int main(int argc, char* argv[])
 {
 	if (argc != 2)
@@ -12,31 +10,15 @@ int main(int argc, char* argv[])
 		return 1;
 	}
 
-	std::string str = argv[1];
 	int upperBound = 0;
-	try
+	if (!GetNumber(argv[1], upperBound))
 	{
-		upperBound = stoi(str);
-	}
-	catch (const std::out_of_range&)
-	{
-		std::cout << "Invalid argument.\n";
+		std::cout << "Invalid number of arguments.\n";
 		std::cout << "Usage: PrimeNumbers.exe <upperBound 1..10^8>\n";
 		return 1;
 	}
-	catch (const std::invalid_argument&)
-	{
-		std::cout << "Invalid argument.\n";
-		std::cout << "Usage: PrimeNumbers.exe <upperBound 1..10^8>\n";
-		return 1;
-	}
-
-	if (upperBound < 1 || upperBound > upperBoundMax)
-	{
-		std::cout << "Invalid argument.\n";
-		std::cout << "Usage: PrimeNumbers.exe <upperBound 1..10^8>\n";
-		return 1;
-	}
+	
+	
 
 	std::set<int> primeNumbersSet = GeneratePrimeNumbersSet(upperBound);
 	std::cout << "to " << upperBound << " contains " << primeNumbersSet.size() << " prime numbers\n";
