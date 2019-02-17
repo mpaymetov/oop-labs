1bbc464ea99583fe4db64438f17814dffeac0087
mpaymetov <muxa147@gmail.com>
2019-02-18 00:20:50 +0300

    изменил тесты, доработал программу

--- lab1/crypt/test.bat
+++ lab1/crypt/test.bat
@@ -3,23 +3,27 @@ set PROGRAM="%~1"
 %PROGRAM% > nul
 if NOT ERRORLEVEL 1 GOTO err
 
+::неверный параметр шифрования
 %PROGRAM% recrypt input.txt output.txt 20 > nul
 if NOT ERRORLEVEL 1 GOTO err
 
-%PROGRAM% crypt input.txt output.txt 283 > nul
-if NOT ERRORLEVEL 1 GOTO err
-
+::несуществующий входной файл
 %PROGRAM% crypt input1.txt output.txt 20 > nul
 if NOT ERRORLEVEL 1 GOTO err
 
-%PROGRAM% crypt input.txt out*put.txt 20 > nul
+::неправильное имя выходного файла
+%PROGRAM% crypt input.txt output*.txt 20 > nul
+if NOT ERRORLEVEL 1 GOTO err
+
+::неверный ключ шифрования
+%PROGRAM% crypt input.txt output.txt 283 > nul
 if NOT ERRORLEVEL 1 GOTO err
 
 %PROGRAM% crypt input.txt output.txt 20 > nul
 if ERRORLEVEL 1 GOTO err
 %PROGRAM% decrypt output.txt decrypt.txt 20 > nul
 if ERRORLEVEL 1 GOTO err
-fc.exe input.txt decrypt.txt > nul
+fc.exe /b input.txt decrypt.txt > nul
 if ERRORLEVEL 1 GOTO err
 
 echo OK
