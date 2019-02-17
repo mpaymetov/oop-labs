f89a72de321f6c5d116a5d12dabe6bc57d3d3f95
mpaymetov <muxa147@gmail.com>
2019-02-17 23:09:15 +0300

    Добавил тесты, доработал программу

--- lab1/radix/test.bat
+++ lab1/radix/test.bat
@@ -11,7 +11,6 @@ rem При запуске без параметров ожидается ненулевой код возврата
 %PROGRAM% > nul
 if NOT ERRORLEVEL 1 goto err
 
-::
 %PROGRAM% 10 10 12 > %OUT%
 if ERRORLEVEL 1 goto err
 fc %OUT% "out.txt" > nul
@@ -19,7 +18,38 @@ if ERRORLEVEL 1 goto err
 
 %PROGRAM% 16 10 1F > %OUT%
 if ERRORLEVEL 1 goto err
-fc %OUT% "out2.txt"
+fc %OUT% "out2.txt" > nul
+if ERRORLEVEL 1 goto err
+
+%PROGRAM% 10 10 1F > %OUT%
+if NOT ERRORLEVEL 1 goto err
+
+%PROGRAM% 1 10 1F > %OUT%
+if NOT ERRORLEVEL 1 goto err
+
+%PROGRAM% 10 37 17 > %OUT%
+if NOT ERRORLEVEL 1 goto err
+
+::sourse radix - stoi argument out of range
+%PROGRAM% 2147483649 10 1F > %OUT%
+if NOT ERRORLEVEL 1 goto err
+
+::sourse radix - not a number
+%PROGRAM% ab 10 6 > %OUT%
+if NOT ERRORLEVEL 1 goto err
+
+::MAX_INT
+%PROGRAM% 16 10 7FFFFFFF > %OUT%
+if ERRORLEVEL 1 goto err
+fc %OUT% "maxint.txt" > nul
+if ERRORLEVEL 1 goto err
+
+%PROGRAM% 16 10 8FFFFFFF > %OUT%
+if NOT ERRORLEVEL 1 goto err
+
+%PROGRAM% 10 8 -35 > %OUT%
+if ERRORLEVEL 1 goto err
+fc %OUT% "m43.txt" > nul
 if ERRORLEVEL 1 goto err
 
 echo OK
