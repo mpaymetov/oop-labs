90bb620bc8399a2d5ccf78c05ceab8c021787b40
mpaymetov <muxa147@gmail.com>
2019-03-07 22:21:40 +0300

    исправил замечания

--- lab2/htmlDecode/htmlDecode.h
+++ lab2/htmlDecode/htmlDecode.h
@@ -3,4 +3,10 @@
 
 typedef std::map<std::string, std::string> HtmlEntitiesMap;
 
+struct PairSearchReplace
+{
+	std::string search;
+	std::string replace;
+};
+
 std::string HtmlDecode(const std::string &html);
